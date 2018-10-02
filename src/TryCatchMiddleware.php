6695166c7f5b81ecb7a92f01b8289976eca5cb37
Marek Bartos <bartos.developer152@gmail.com>
2018-10-02 15:41:49 +0200

    Support logging in TryCatchMiddleware

--- src/TryCatchMiddleware.php
+++ src/TryCatchMiddleware.php
@@ -4,6 +4,8 @@ namespace Contributte\Middlewares;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Throwable;
 
 class TryCatchMiddleware implements IMiddleware
@@ -12,13 +14,23 @@ class TryCatchMiddleware implements IMiddleware
 	/** @var bool */
 	private $catch = true;
 
+	/** @var bool */
+	private $debug = false;
+
+	/** @var LoggerInterface|null */
+	private $logger;
+
+	/** @var string */
+	private $logLevel;
+
 	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
 	{
-		if ($this->catch) {
+		if (!$this->debug || $this->catch) { // Always catch if not debug and catch optionally if is debug
 			try {
 				$response = $next($request, $response);
 				return $response;
 			} catch (Throwable $throwable) {
+				$this->log($throwable);
 				$response = $response->withStatus(500);
 				$response->getBody()->write('Application encountered an internal error. Please try again later.');
 				return $response;
@@ -31,6 +43,24 @@ class TryCatchMiddleware implements IMiddleware
 	public function setCatchExceptions(bool $catch): void
 	{
 		$this->catch = $catch;
+	}
+
+	public function setDebugMode(bool $debug): void
+	{
+		$this->debug = $debug;
+	}
+
+	public function setLogger(LoggerInterface $logger, string $logLevel = LogLevel::ERROR): void
+	{
+		$this->logger = $logger;
+		$this->logLevel = $logLevel;
+	}
+
+	private function log(Throwable $throwable): void
+	{
+		if ($this->logger === null) return;
+
+		$this->logger->log($throwable->getMessage(), $this->logLevel);
 	}
 
 }
